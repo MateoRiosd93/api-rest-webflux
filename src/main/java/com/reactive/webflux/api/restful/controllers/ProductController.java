f7fa48e3f59a3affb2f08156cf805ae13a73b752
marios <marios@bancolombia.com.co>
2025-05-19 15:35:35 -0500

    feat(rest): se implementa delete y upload para cargar una image al product

--- src/main/java/com/reactive/webflux/api/restful/controllers/ProductController.java
+++ src/main/java/com/reactive/webflux/api/restful/controllers/ProductController.java
@@ -3,20 +3,27 @@ package com.reactive.webflux.api.restful.controllers;
 import com.reactive.webflux.api.restful.models.documents.Product;
 import com.reactive.webflux.api.restful.services.product.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
+    @Value("{config.uploads.path}")
+    private String path;
 
     @GetMapping
     public Mono<ResponseEntity<Flux<Product>>> getProducts() {
@@ -68,4 +75,28 @@ public class ProductController {
                 )
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteProduct(@PathVariable String id) {
+        return productService.findById(id)
+                .flatMap(product -> productService.delete(product).then(Mono.just((new ResponseEntity<Void>(HttpStatus.NO_CONTENT)))))
+                .defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping("/upload/{id}")
+    public Mono<ResponseEntity<Product>> uploadImage(@PathVariable String id, @RequestPart FilePart filePart) {
+        return productService.findById(id)
+                .flatMap(product -> {
+                    product.setImage(UUID.randomUUID().toString() + "-" + filePart.filename()
+                            .replace(" ", "")
+                            .replace(":", "")
+                            .replace("\\", ""));
+
+                    return filePart.transferTo(new File(path + product.getImage())).then(productService.save(product));
+                })
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
 }

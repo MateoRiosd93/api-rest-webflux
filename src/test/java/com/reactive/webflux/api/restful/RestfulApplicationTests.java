70b9de2f6155e53753901b51b568c91ac79a1b30
marios <marios@bancolombia.com.co>
2025-05-21 11:25:02 -0500

    feat(rest): Se realiza test para el handler de getProducts

--- src/test/java/com/reactive/webflux/api/restful/RestfulApplicationTests.java
+++ src/test/java/com/reactive/webflux/api/restful/RestfulApplicationTests.java
@@ -1,13 +1,28 @@
 package com.reactive.webflux.api.restful;
 
+import com.reactive.webflux.api.restful.models.documents.Product;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestfulApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	public void getProducts() {
+		webTestClient.get()
+				.uri("/api/handler/products")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Product.class);
 	}
 
 }
